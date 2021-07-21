34e92f40212532edaa83b23be4a02c20fecb4c49
ollymonger <olivermonger123@gmail.com>
2021-07-21 22:08:34 +0100

    Spell checker added

--- src/components/events/onMessage.ts
+++ src/components/events/onMessage.ts
@@ -8,49 +8,37 @@ const dictionary = fs.readFileSync('src/components/events/dictionary/en-US.dic')
 
 const nodehun = new Nodehun(affix, dictionary);
 
-
 export async function onMessage(message: ArgsOf<"message">, client: Client) {
     if (message[0].author !== client.user) {
         const messageSplit = message[0].content.split(" ");
-        let incorrect = 0;
-
-        messageSplit.map(async m => {
-            const spell = await nodehun.spell(m);
-        })
-    }
-}
-
-/*
-const sendEmbed = async (messageSplit, message) => {
-    let str = "";
-
-    messageSplit.map(async m => {
-        if (str == "") {
-            if (await nodehun.spell(m)) {
-                str = m;
+        let str = "";
+        let int = 0;
+        try {
+            for(const mes of messageSplit){
+                const spelledCorrectly = await nodehun.spell(mes);
+                if(spelledCorrectly && !message[0].content.includes("@")) {
+                    str += ` ${mes}`;
+                } else if(!spelledCorrectly && !message[0].content.includes("@")) {
+                    const suggestion = await nodehun.suggest(mes);
+                    str += ` ${suggestion[0]}`;
+                    int++;
+                }
+            }
+            if(int !== 0){
+                let embed: MessageEmbed[] = [new MessageEmbed()];
+                embed[0].setTitle("Ollbot - spell check");
+                embed[0].setDescription("Loading...");
+    
+                let reply = await message[0].channel.send({ embeds: embed });
+    
+                embed[0].setTitle("Ollbot - spell check");
+                embed[0].setDescription(`${message[0].author}:${str}`);
+                await reply.edit({ embeds: embed });
+                message[0].delete();
                 return;
             }
-            let array = await nodehun.suggest(m)
-            str = await array[0];
+        } catch (e) {
+            console.error(e.message);
         }
-        if (await nodehun.spell(m)) {
-            str += ` ${m}`;
-            return;
-        }
-        let array = await nodehun.suggest(m)
-        str += ` ${await array[0]}`;
-    })
-
-    let embed: MessageEmbed[] = [new MessageEmbed()];
-    embed[0].setTitle("Ollbot - spell check");
-    embed[0].setDescription("Loading...");
-
-    let reply = await message[0].channel.send({ embeds: embed });
-
-    embed[0].setTitle("Ollbot - spell check");
-    embed[0].setDescription(`${str}`);
-    await reply.edit({ embeds: embed });
-
-    return;
+    }
 }
-*/
