6c423d154ca11be5c57cd3f96ccd55e74836f39c
Olly Monger <olivermonger123@gmail.com>
2021-07-21 01:04:59 +0100

    OnMessage event created, send embedded message to correct spelling

--- src/components/events/onMessage.ts
+++ src/components/events/onMessage.ts
@@ -1,1 +1,15 @@
+import { ArgsOf, Client } from '@typeit/discord';
+import { ClientUser, MessageEmbed } from 'discord.js';
 import * as spellchecker from 'simple-spellchecker';
+import { Index } from '../..';
+
+
+export async function onMessage(message: ArgsOf<"message">, client: Client) {
+    if (message[0].author !== client.user) {
+        let embed: MessageEmbed[] = [new MessageEmbed()];
+        embed[0].setTitle("Spelling Corrected");
+        embed[0].setDescription(`${message[0].content}`);
+        message[0].channel.send({ embeds: embed });
+        return;
+    }
+}
--- src/components/handler/Events.ts
+++ src/components/handler/Events.ts
@@ -1,7 +1,8 @@
-import { Discord, On, Guild } from "@typeit/discord";
+import { Discord, On, Guild, Client, ArgsOf } from "@typeit/discord";
 import { GuildType } from "../models/GuildType";
 import { onJoin } from '../events/onJoin';
 import { ExtendedGuildType } from "../models/ExtendedGuildType";
+import { onMessage } from "../events/onMessage";
 
 @Discord()
 @Guild()
@@ -10,6 +11,15 @@ export abstract class Events {
     async join(guild: GuildType): Promise<void> {
         try {
             await onJoin(guild[0]);
+        } catch (e) {
+            return console.log(e.message);
+        }
+    }
+
+    @On("messageCreate")
+    async message(message: ArgsOf<"message">, client: Client): Promise<void> {
+        try {
+            await onMessage(message, client);
         } catch (e) {
             return console.log(e.message);
         }
